4f97750d739562829d453fcb0f92da23028174db
Gopika-3747 <gopikavb@gmail.com>
2025-07-10 00:31:32 +0530

    ..

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      <h1 className="p-30 bg-amber-200">Gopika</h1>
+    <div>
+      <h1 className="p-30 bg-amber-200 text-center font-bold text-4xl">Power Share</h1>
     </div>
   )
 }

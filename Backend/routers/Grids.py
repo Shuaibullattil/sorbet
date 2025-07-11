a63ade7e4648445f4dcbfeff9bc952afb695fca5
Sensei3747 <vivekofficial619@gmail.com>
2025-07-11 16:13:48 +0530

    Grid retrieval api

--- Backend/routers/Grids.py
+++ Backend/routers/Grids.py
@@ -54,6 +54,29 @@ async def insert_new_grid(grid: UserGrid,token: str = Depends(oauth2_scheme)):
         "grid_id": str(result.inserted_id)
     }
     
-    
+@router.get("/get_user_grid")
+async def get_user_grid(token: str = Depends(oauth2_scheme)):
+    payload = decode_token(token)
+    email = payload.get("sub")
+    if not email:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user = collection.find_one({"email": email}, {"_id": 1, "name": 0, "email": 0, "mobile": 0, "password": 0})
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found")
+    grid = Grid_Collection.find_one({"user": ObjectId(user["_id"])})
+    if not grid:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Grid not found for this user"
+        )
+    grid["_id"] = str(grid["_id"])
+    grid["user"] = str(grid["user"])
+    return grid
 
 

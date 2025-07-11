43aebd0d732b1fbe08de3f8355e37c481825d4db
Sensei3747 <vivekofficial619@gmail.com>
2025-07-11 12:08:03 +0530

    CORS MIDDLEWARE

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import Users
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (use ["http://localhost:3000"] for security)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(Users.router)
 
